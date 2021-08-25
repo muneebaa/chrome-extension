e7da0e66de38fbefb9a21a18d09b74f4d479b953
Muneeba Dilawaze <muneeba.dilawaze@gmail.com>
2021-08-25 22:41:23 +0500

    initial commit

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads") || '[]'); // Stacksnippets do not allow localStorage
 const tabBtn = document.getElementById("tab-btn")
+const iconEl = document.getElementById("icon")
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function (tabs) {
         myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
         render(myLeads)
     })
 })
@@ -26,22 +30,37 @@ function render(leads) {
             <li>
                 <a target='_blank' href='${leads[i]}'>
                     ${leads[i]}
-                </a>
+                </a><i class="ri-close-circle-line delete"></i>
             </li>
         `
     }
     ulEl.innerHTML = listItems
 }
 
-deleteBtn.addEventListener("click", function() {
+deleteBtn.addEventListener("click", function () {
     localStorage.clear()
     myLeads = []
     render(myLeads)
 })
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
+inputBtn.addEventListener("click", function () {
+    if (inputEl.value) {
+        myLeads.push(inputEl.value)
+        inputEl.value = ""
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    }
+})
+
+ulEl.addEventListener("click", function (e) {
+    const tgt = e.target;
+    if (tgt.classList.contains("delete")) {
+        const task = tgt.closest("li");
+        const url = task.querySelector("a").getAttribute("href"); // important
+        const idx = myLeads.findIndex(item => item === url);
+        myLeads.splice(idx, 1)
+        console.log(myLeads)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        task.remove()
+    }
 })
